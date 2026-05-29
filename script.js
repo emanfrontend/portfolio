98bda64e8578944166694e0719aef1923117e3ee
Eman Mahmoud Abo Hassan <eman.web2826@gmail.com>
2026-05-29 10:30:05 +0100

    Upload portfolio files

---
+++ script.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll(".navbar a");
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.forEach(item => {
+            item.classList.remove("active");
+        });
+
+        link.classList.add("active");
+
+    });
+
+});
+
+console.log("Portfolio Loaded Successfully");
